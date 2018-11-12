db247a2837c2e8830c649d11f94580f3a19f2863
Joe 1 <jo1@example.com>
2018-11-12 15:54:01 +0000

    first verion

--- resources.tf
+++ resources.tf
@@ -1,9 +1,21 @@
-resource "aws_vpc" "environement-example-two" {
- cidr_block = "10.0.0.0/16"
- enable_dns_hostnames = true
- enable_dns_support = true
- tags {
-  Name = "terraform-aws-vpc-example2"
- }
-
+resource "aws_vpc" "environment-example-two" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "terraform-aws-vpc-example-two"
+  }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2b"
+}
+
